1a2779d5e054c1419f4bb2caf9ba7ea12554b892
Dani4734 <danielvillasanti19@gmail.com>
2023-02-28 19:54:24 -0300

    commit 4.0.9

---
+++ java/main.js
@@ -0,0 +1,15 @@
+let miPerfil = document.getElementById('miLinkPerfil');
+let miPortafolio = document.getElementById('miLinkPortafolio');
+let contacto = document.getElementById('miLinkContacto');
+
+
+//let clikNav = document.addEventListener('clik', miPerfil);
+//    window.location.href = 
+
+
+//bucle para ver cantidad de selectores, cases, id, container
+ /*   for (var i = 0;i < id.length; i++) {
+        id[i].id = "enlaces"+i;
+
+    }*/
+
